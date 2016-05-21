17b68d8012104ffaeb8660467117e16688479738
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-05-21 19:25:16 +0200

    Create domain class Person

---
+++ app/src/main/java/com/example/malkomich/kotlinsample/KotlinPerson.kt
@@ -0,0 +1,9 @@
+package com.example.malkomich.kotlinsample
+
+/**
+ * Created by malkomich on 21/05/2016.
+ */
+data class KotlinPerson(
+        var name: String,
+        var surname: String,
+        var age: Integer)
