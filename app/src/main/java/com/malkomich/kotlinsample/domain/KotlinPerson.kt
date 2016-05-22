0e3afb159781160bec6c975fe9d71a55026053fd
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-05-22 15:09:30 +0200

    Remove unused domain class Person

--- app/src/main/java/com/malkomich/kotlinsample/domain/KotlinPerson.kt
+++
@@ -1,9 +0,0 @@
-package com.malkomich.kotlinsample.domain
-
-/**
- * Created by malkomich on 21/05/2016.
- */
-data class KotlinPerson(
-        var name: String,
-        var surname: String,
-        var age: Integer)
